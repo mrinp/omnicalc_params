e7f931306c12ebab69b044b0ddbf5b72aa7020e4
mrinp <mrinp@users.noreply.github.com>
2017-04-20 18:21:08 -0500

    First set of updates

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,58 @@
+class CalculationsController <ApplicationController
+
+  def flex_square
+    #Parameters: {"number"=>"5"}
+    @user_input = params["number"].to_f #always called params
+    @square = @user_input**2
+    render ("calculations/flex_square.html.erb")
+  end
+
+  def flex_square_root
+    #Parameters: {"number"=>"5"}
+    @user_input = params["number"].to_f #always called params
+    @square_root = (@user_input**0.5).round(2)
+    render ("calculations/flex_square_root.html.erb")
+  end
+
+  def flex_payment
+    @years = params["years"].to_f
+    @principal = params["principal"].to_f
+    @apr = (params["apr"].to_f)/100
+    @monthly_payment = ( ((@apr/100/12)*@principal) /( 1- ((1+(@apr/100/12)) ** (@years*-12)) )).round(2)
+
+    render ("calculations/flex_payment.html.erb")
+  end
+
+  def square_form
+    render ("calculations/square_form.html.erb")
+  end
+
+  def square
+    @user_input = params["user_number"].to_f
+    @square = (@user_input**2)
+    render ("calculations/square_results.html.erb")
+  end
+
+  def square_root_form
+    render ("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @user_input = params["user_number"].to_f
+    @square_root = (@user_input**0.5).round(2)
+    render ("calculations/square_root_results.html.erb")
+  end
+
+  def payment_form
+    render ("calculations/payment_form.html.erb")
+  end
+
+  def payment
+    @years = params["years"].to_f
+    @principal = params["principal"].to_f
+    @apr = (params["apr"].to_f)
+    @monthly_payment = ( ((@apr/100/12)*@principal) /( 1- ((1+(@apr/100/12)) ** (@years*-12)) )).round(2)
+    render ("calculations/payment_results.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get("/flexible/square/:number", {:controller => "calculations", :action => "flex_square"})
+  get("/flexible/square_root/:number", {:controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:apr/:years/:principal", {:controller => "calculations", :action => "flex_payment"})
+
+  get("/square/new", {:controller => "calculations", :action => "square_form"})
+  get("/square/results", {:controller => "calculations", :action => "square"})
+
+  get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+  get("/square_root/results", {:controller => "calculations", :action => "square_root"})
+
+  get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+  get("/payment_results", {:controller => "calculations", :action => "payment"})
+
+  #the :number above is accepting everything as a string
 end
