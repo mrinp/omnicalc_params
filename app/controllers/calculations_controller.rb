226cb14d3703b70e22f989590f5b914862ae390e
mrinp <mrinp@users.noreply.github.com>
2017-04-24 17:52:05 -0500

    All Forms Completed
    Minor tweaks still required

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -23,6 +23,14 @@ class CalculationsController <ApplicationController
     render ("calculations/flex_payment.html.erb")
   end
 
+  def flex_random
+    @min = params["min"].to_f
+    @max = params["max"].to_f
+    @random = rand(@min..@max).round(2)
+
+    render ("calculations/flex_random.html.erb")
+  end
+
   def square_form
     render ("calculations/square_form.html.erb")
   end
@@ -50,9 +58,21 @@ class CalculationsController <ApplicationController
   def payment
     @years = params["years"].to_f
     @principal = params["principal"].to_f
-    @apr = (params["apr"].to_f)
+    @apr = (params["apr"].to_f)/100
     @monthly_payment = ( ((@apr/100/12)*@principal) /( 1- ((1+(@apr/100/12)) ** (@years*-12)) )).round(2)
+
     render ("calculations/payment_results.html.erb")
+  end
+
+  def random_form
+    render ("calculations/random_form.html.erb")
+  end
+
+  def random_results
+    @min = params["min"].to_f
+    @max = params["max"].to_f
+    @random = rand(@min..@max).round(4)
+    render ("calculations/random_results.html.erb")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get("/flexible/square/:number", {:controller => "calculations", :action => "flex_square"})
   get("/flexible/square_root/:number", {:controller => "calculations", :action => "flex_square_root"})
   get("/flexible/payment/:apr/:years/:principal", {:controller => "calculations", :action => "flex_payment"})
+  get("/flexible/random/:min/:max", {:controller => "calculations", :action => "flex_random"})
 
   get("/square/new", {:controller => "calculations", :action => "square_form"})
   get("/square/results", {:controller => "calculations", :action => "square"})
@@ -10,7 +11,10 @@ Rails.application.routes.draw do
   get("/square_root/results", {:controller => "calculations", :action => "square_root"})
 
   get("/payment/new", {:controller => "calculations", :action => "payment_form"})
-  get("/payment_results", {:controller => "calculations", :action => "payment"})
+  get("/payment_results/results", {:controller => "calculations", :action => "payment"})
+
+  get("/random/new", {:controller => "calculations", :action => "random_form"})
+  get("/random_results", {:controller => "calculations", :action => "random_results"})
 
   #the :number above is accepting everything as a string
 end
